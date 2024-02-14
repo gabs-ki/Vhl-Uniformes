e34fb23a1e1cae2ed5d4562e20fac8560b2f2389
Marcelo G <log4304.016@gmail.com>
2024-02-14 14:15:46 -0300

    jaisdj

--- VHL/src/pages/home/Home.jsx
+++ VHL/src/pages/home/Home.jsx
@@ -22,38 +22,16 @@ import TituloContato from './images/tituloContatos.svg'
 import FundoCatalogo from './images/fundoCatalogo.svg'
 import TituloCatalogo from './images/tituloCatalogo.svg'
 import '../home/carousel.scss'
-import Macacao from './images/listaDeProdutos/macacao.svg'
-import CamisaPolo from './images/listaDeProdutos/camisaPolo.svg'
-import MangaLongaSocial from './images/listaDeProdutos/mangaLongaSocial.svg'
-import Swiper from 'swiper';
-import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import Macacao from './images/listaDeProdutos/macacao2.svg'
+import CamisaPolo from './images/listaDeProdutos/camisetaPolo2.svg'
+import MangaLongaSocial from './images/listaDeProdutos/camisaSocial2.svg'
+import BotaoDireita from './images/botaoDireita.svg'
+import BotaoEsquerda from './images/botaoEsquerda.svg'
+import SetaCategoria from './images/setaCategoria.svg'
 
-import 'swiper/css'
-import 'swiper/css/navigation'
-import 'swiper/css/pagination'
-import 'swiper/css/scrollbar'
-import 'swiper/element/css/autoplay'
-
-import { register } from 'swiper/element/bundle';
-// register Swiper custom elements
-register();
+import { Link, animateScroll as scroll } from "react-scroll";
 
 function Home() {
-
-    const swiper = new Swiper('.swiper', {
-
-        modules: [Navigation, Pagination, Autoplay],
-
-        autoplay: {
-            delay: 5000,
-        },
-
-    });
-
-
-    const [firstSwiper, setFirstSwiper] = useState(null)
-    const [secondSwiper, setSecondSwiper] = useState(null)
-
 
     const [barraAberta, setBarraAberta] = useState(true)
 
@@ -61,26 +39,28 @@ function Home() {
 
     const [items, setItems] = useState(1, 2, 3)
 
-    const [active, setActive] = useState(0)
+    const [active, setActive] = useState(1)
 
     const [direction, setDirection] = useState('')
 
     const [arrayItems, setArrayItems] = useState([])
 
-    const [images, setImages] = useState([Macacao, MangaLongaSocial, CamisaPolo])
-
-
+    const [images, setImages] = useState([
+        { img: Macacao, desc: 'Uniformes Profissionais', link: 'https://api.whatsapp.com/send?phone=5511947850224&text=Bom%20dia!%20Gostaria%20de%20saber%20mais%20sobre%20os%20Uniformes%20Profissionais.' },
+        { img: MangaLongaSocial, desc: 'Uniformes Sociais', link: 'https://api.whatsapp.com/send?phone=5511947850224&text=Bom%20dia!%20Gostaria%20de%20saber%20mais%20sobre%20os%20Uniformes%20Sociais.' },
+        { img: CamisaPolo, desc: 'Camiseta Personalizadas', link: 'https://api.whatsapp.com/send?phone=5511947850224&text=Bom%20dia!%20Gostaria%20de%20saber%20mais%20sobre%20as%20Camisetas%20Personalizadas.' }
+    ])
 
     const Carousel = () => {
 
         var letArrayItems = []
         var level
 
-        console.log(active)
+        console.log(images)
 
         images.map((item, index) => {
-
-            letArrayItems.push({ key: index, id: index, level: active, image: item })
+            console.log(item)
+            letArrayItems.push({ image: item.img, categoria: item.desc, link: item.link })
 
         })
 
@@ -90,8 +70,51 @@ function Home() {
 
     useEffect(() => {
         Carousel()
+
     }, [active])
 
+    useEffect(() => {
+
+        const timeoutId = setTimeout(() => {
+            CliqueDireito()
+        }, 4000);
+
+
+        return () => clearTimeout(timeoutId);
+    })
+
+    const CliqueEsquerdo = () => {
+
+        const letArrayItems = [...arrayItems]
+
+        letArrayItems.map((item, index) => {
+            if (index == arrayItems.length - 1) {
+                var itemSplice = letArrayItems.splice(index, 1)
+                letArrayItems.unshift(itemSplice[0])
+
+            }
+        })
+
+        setArrayItems(letArrayItems)
+    }
+
+    const CliqueDireito = () => {
+        const letArrayItems = [...arrayItems]
+
+        letArrayItems.map((item, index) => {
+            if (index == arrayItems.length - 3) {
+                var itemSplice = letArrayItems.splice(index, 1)
+                letArrayItems.push(itemSplice[0])
+                console.log(letArrayItems)
+            }
+        })
+
+        setArrayItems(letArrayItems)
+    }
+
+    useEffect(() => {
+
+    }, [])
 
     return (
 
@@ -121,12 +144,49 @@ function Home() {
                         <div className={`${barraAberta ? 'barraNavegacaoMenu' : 'barraNavegacaoMenuAberta'}`}>
 
                             <div className='barraNavegacaoMenu__opcaoFecharAbrir'>
+
                                 <span className='textoGrifadoMenu'>MENU</span>
 
                             </div>
 
                             <div className='barraNavegacaoMenu__opcoesDeNavegacao'>
-                                <span className='opcaoHome'>HOME</span>
+
+                                <Link className='opcaoHome'
+                                    to='home'
+                                    spy={true}
+                                    smooth={true}
+                                    offset={-70}
+                                    duration={500}
+                                >
+                                    HOME
+                                </Link>
+                                <Link className='opcaoHome'
+                                    to='informacoes'
+                                    spy={true}
+                                    smooth={true}
+                                    offset={90}
+                                    duration={500}
+                                >
+                                    INFORMAÇÕES
+                                </Link>
+                                <Link className='opcaoHome'
+                                    to='catalogo'
+                                    spy={true}
+                                    smooth={true}
+                                    offset={90}
+                                    duration={500}
+                                >
+                                    CATEGORIAS
+                                </Link>
+                                <Link className='opcaoHome'
+                                    to='contatos'
+                                    spy={true}
+                                    smooth={true}
+                                    offset={0}
+                                    duration={500}
+                                >
+                                    CONTATOS
+                                </Link>
 
 
                             </div>
@@ -145,8 +205,14 @@ function Home() {
                             <p className='textoNormalHomeDois'>Não perca tempo e faça já o seu pedido!</p>
 
                             <div className='botoesHome'>
-                                <button className='botaoHomePrincipal'> ENTRE EM CONTATO </button>
-                                <img className='botaoHomeSecundario' src={BotaoSecundarioHome} alt="" />
+                                <a target='_blank' href="https://api.whatsapp.com/send?phone=5511947850224"><button className='botaoHomePrincipal'> ENTRE EM CONTATO </button></a>
+                                <Link
+                                    to='catalogo'
+                                    spy={true}
+                                    smooth={true}
+                                    offset={100}
+                                    duration={500}
+                                ><img className='botaoHomeSecundario' src={BotaoSecundarioHome} alt="" /></Link>
                             </div>
 
                         </div>
@@ -167,9 +233,9 @@ function Home() {
 
                         <span className='conhecaRedes'>CONHEÇA-NOS MAIS</span>
 
-                        <div className='rede WhatsApp'><i class="fab fa-whatsapp"></i></div>
-                        <div className='rede Facebook'><i class="fab fa-facebook-f"></i></div>
-                        <div className='rede Instagram'><i class="fab fa-instagram"></i></div>
+                        <a target='_blank' href="https://api.whatsapp.com/send?phone=5511947850224"><div className='rede WhatsApp'><i class="fab fa-whatsapp"></i></div></a>
+                        {/* <a href=""><div className='rede Facebook'><i class="fab fa-facebook-f"></i></div></a> */}
+                        {/* <a href=""><div className='rede Instagram'><i class="fab fa-instagram"></i></div></a> */}
 
                         <div className='linhaRedesDois'></div>
 
@@ -204,7 +270,7 @@ function Home() {
                             <img src={TituloSegundoParagrafo} alt="" />
                             <div className='linhaVisitaUm'></div>
                             <p className='textoInformacoesVisita'>Para colabores e clientes que estão perto de nós, podemos fazer uma visita!</p>
-                            <button className='botaoAgendarVisita'>AGENDE UMA VISITA</button>
+                            <a target='_blank' href="https://api.whatsapp.com/send?phone=5511947850224"><button className='botaoAgendarVisita'>AGENDE UMA VISITA</button></a>
 
                         </div>
 
@@ -230,13 +296,14 @@ function Home() {
                     </div>
 
                     <div className='containerCatalogo__listaDeProdutos'>
-                        <swiper-container
+                        {/* <swiper-container
                             slides-per-view="1"
                             navigation="true"
                             pagination="true"
                             scrollbar="true"
-                            centeredSlides={true}
-                            autoplay={100}
+                            
+                            autoplay="1000"
+                            
                         >
                             {
                                 arrayItems.map((item, index) => (
@@ -250,8 +317,66 @@ function Home() {
                                 ))
                             }
 
-                        </swiper-container>
+                        </swiper-container> */}
 
+                        <div className='listaItems'>
+                            <i className='botaoEsquerda'
+                                onClick={() => {
+
+                                    CliqueEsquerdo()
+
+                                }}>
+                                <img src={BotaoEsquerda} alt="" />
+                            </i>
+
+                            {
+
+                                arrayItems.map((item, index) => {
+                                    if (index == active) {
+
+                                        return (
+                                            <div className='item'>
+                                                <span className='tituloCategoriaProdutos'>
+                                                    <img src={SetaCategoria} alt="" />
+                                                    {item.categoria}
+                                                </span>
+                                                <img className='imageActive' src={item.image} alt="" />
+                                                <a target='_blank' href={item.link}>
+                                                    <div className='botaoItem'>PEÇA JÁ</div>
+                                                </a>
+                                            </div>
+                                        )
+
+                                    } else {
+                                        return (
+                                            <div>
+                                                <img className='imageInactive' src={item.image} alt="" />
+                                            </div>
+                                        )
+                                    }
+                                })
+
+                            }
+
+                            <i className='botaoDireita'
+                                onClick={() => {
+
+                                    // var newActive = active + 1
+
+                                    // if(newActive >= arrayItems.length) {
+                                    //     setActive(0)
+                                    // } else {
+                                    //     setActive(newActive)
+                                    // }
+
+                                    CliqueDireito()
+
+
+                                }}
+                            >
+                                <img src={BotaoDireita} alt="" />
+                            </i>
+                        </div>
 
                     </div>
                 </section>
@@ -263,11 +388,11 @@ function Home() {
                     <img src={TituloContato} alt="" />
 
                     <div className='containerContatos__conteudoContatos'>
-                        <span>Telefone: <a href=""><b>+55 (11) 3683-7002</b></a></span>
-                        <span>E-Mail: <a href=""><b>vendas@vhluniformes.com</b></a></span>
+                        <span>Telefone: <a target='_blank' href="https://www.sejda.com/call/01136837002"><b>+55 (11) 3683-7002</b></a></span>
+                        <span>E-Mail: <a target='_blank' href=""><b>vendas@vhluniformes.com</b></a></span>
                     </div>
 
-                    <span>Endereço: <a href=""><b>Av. Francisco Lourenço da Costa, 612 - Vila do Conde, Barueri - SP, 06445-500</b></a></span>
+                    <span>Endereço: <a target='_blank' href="https://www.google.com/maps/place/Av.+Francisco+Louren%C3%A7o+da+Costa,+612+-+Vila+do+Conde,+Barueri+-+SP,+06445-500/@-23.5322834,-46.8658102,19z/data=!3m1!4b1!4m5!3m4!1s0x94cf010ab5c34e1f:0xba89a5a05f4ed05d!8m2!3d-23.5322846!4d-46.8651651?entry=ttu"><b>Av. Francisco Lourenço da Costa, 612 - Vila do Conde, Barueri - SP, 06445-500</b></a></span>
 
                     <div className='conteudoContatos_conhecaNosMais'>
 
@@ -277,10 +402,10 @@ function Home() {
 
                         <div className='contatosRedes'>
 
-                            <div className='icone Email'><i class="far fa-envelope"></i></div>
-                            <div className='icone WhatsApp'><i class="fab fa-whatsapp"></i></div>
-                            <div className='icone Facebook'><i class="fab fa-facebook-f"></i></div>
-                            <div className='icone Instagram'><i class="fab fa-instagram"></i></div>
+                            <a target='_blank' href="mailto:vendas@vhluniformes.com"><div className='icone Email'><i class="far fa-envelope"></i></div></a>
+                            <a target='_blank' href="https://api.whatsapp.com/send?phone=5511947850224"><div className='icone WhatsApp'><i class="fab fa-whatsapp"></i></div></a>
+                            {/* <div className='icone Facebook'><i class="fab fa-facebook-f"></i></div> */}
+                            {/* <div className='icone Instagram'><i class="fab fa-instagram"></i></div> */}
 
                         </div>
 
@@ -288,7 +413,7 @@ function Home() {
 
                     </div>
 
-                    <span>VHL UNIFORMES © 2024 Todos os direitos reservados</span>
+                    <span><b>VHL UNIFORMES © 2024</b> Todos os direitos reservados</span>
 
                 </section>
 
