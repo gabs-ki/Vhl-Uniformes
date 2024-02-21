fdb11696a58ea0529ea32b7ff8c2d65cb840f604
marceloGabriel <log4304.016@gmail.com>
2024-02-21 17:28:44 -0300

    asfasf

--- VHL/src/pages/home/Home.jsx
+++ VHL/src/pages/home/Home.jsx
@@ -269,7 +269,7 @@ function Home() {
 
                             <img className='tituloVisita' src={TituloSegundoParagrafo} alt="" />
                             <div className='linhaVisitaUm'></div>
-                            <p className='textoInformacoesVisita'>Para colabores e clientes que estão perto de nós, podemos fazer uma visita!</p>
+                            <p className='textoInformacoesVisita'>Agende conosco uma visita ao seu estabelecimento, onde poderemos conhecer melhor vocês colaboradores!</p>
                             <a target='_blank' href="https://api.whatsapp.com/send?phone=5511947850224"><button className='botaoAgendarVisita'>AGENDE UMA VISITA</button></a>
 
                         </div>
